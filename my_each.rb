f6703b62f35c3e899b2931c3eb7d96fffbd54fe9
Audra Tallman <audra.tallman@hotmail.com>
2019-03-04 15:20:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,4 @@
-def my_each (words)
-  while words.length < 5 do |word|
-    puts word
-    counter +=1
-    times_called +=1
-  end   
+def my_each (collection)
+  while i > 0 do |i|
+    puts i
 end
